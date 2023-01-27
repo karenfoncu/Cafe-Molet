3dd8630745d80adaeff22bef674e426d1516c216
karenfoncu <120802490+karenfoncu@users.noreply.github.com>
2023-01-27 12:23:28 -0300

    Add files via upload

---
+++ Cafeteria Molet/js/script.js
@@ -0,0 +1,20 @@
+$('.btn-nuestro-menu').on('click', function(){
+
+    $('.pagina-izquierda').addClass("pagina-izquierda2");
+    setTimeout(function(){
+        $('.pagina-derecha').addClass("pagina-derecha2");
+    }, 300)
+   
+})
+
+$('.btn-cerrar').on('click', function(){
+
+    $('.pagina-derecha').removeClass("pagina-derecha2");
+    
+    setTimeout(function(){
+
+    $('.pagina-izquierda').removeClass("pagina-izquierda2");
+    
+    }, 300)
+   
+})
